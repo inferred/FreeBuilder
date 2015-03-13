c89ea94e2e98f46ff8351c092bcbdc8e70723b8d
Chris Purcell <Chris.Purcell.39@gmail.com>
2015-03-13 23:57:00 +0000

    Add skeleton of new SourceLevel enum, to support emitting idiomatic Java 7 code.

---
+++ src/main/java/org/inferred/freebuilder/processor/SourceLevel.java
@@ -0,0 +1,26 @@
+package org.inferred.freebuilder.processor;
+
+import javax.lang.model.SourceVersion;
+
+import org.inferred.freebuilder.processor.util.TypeReference;
+
+import com.google.common.base.Optional;
+
+/**
+ * Compliance levels which are idiomatically supported by this processor.
+ *
+ * <p>{@link SourceVersion} is problematic to use, as the constants themselves will be missing
+ * on compilers that do not support them (e.g. "RELEASE_8" is not available on javac v6 or v7).
+ */
+public enum SourceLevel {
+  JAVA_6, JAVA_7;
+
+  public static SourceLevel from(SourceVersion sourceVersion) {
+    // RELEASE_6 is always available, as previous releases did not support annotation processing.
+    if (sourceVersion.compareTo(SourceVersion.RELEASE_6) <= 0) {
+      return JAVA_6;
+    } else {
+      return JAVA_7;
+    }
+  }
+}
