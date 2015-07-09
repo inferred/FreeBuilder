6b4bea5b2b6ac991257c3596c276deb7cee2c324
Chris Purcell <Chris.Purcell.39@gmail.com>
2015-07-09 09:28:45 +0100

    Add a GWT integration test suite to prevent future regressions.

---
+++ src/it/gwt/src/test/java/org/inferred/freebuilder/server/rpc/NestedListTestServiceImpl.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.inferred.freebuilder.server.rpc;
+
+import org.inferred.freebuilder.client.rpc.NestedListGwtType;
+import org.inferred.freebuilder.client.rpc.NestedListTestService;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class NestedListTestServiceImpl extends RemoteServiceServlet implements NestedListTestService {
+
+    @Override
+    public NestedListGwtType echo(NestedListGwtType mapGwtType) {
+        return mapGwtType;
+    }
+}
