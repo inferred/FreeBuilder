2857151900bd265433d1f4727d11ac3107cd5db7
Chris Purcell <Chris.Purcell.39@gmail.com>
2015-10-27 23:47:57 +0000

    Use google-java-format on generated code
    Generated code was already mostly inline with the Google readability standard, but minor deviations (mainly around getting line lengths optimal) have resulted in lots of cosmetic changes to the CodeGeneratorTest gold standard.

--- src/main/java/org/inferred/freebuilder/processor/util/CompilationUnitWriter.java
+++ src/main/java/org/inferred/freebuilder/processor/util/CompilationUnitWriter.java
@@ -15,6 +15,9 @@
  */
 package org.inferred.freebuilder.processor.util;
 
+import com.google.googlejavaformat.java.Formatter;
+import com.google.googlejavaformat.java.FormatterException;
+
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.Writer;
@@ -106,9 +109,11 @@ public class CompilationUnitWriter implements SourceBuilder, Closeable {
         }
         writer.append("\n");
       }
-      writer.append(source.toString());
+      writer.append(new Formatter().formatSource(source.toString()));
       writer.close();
     } catch (IOException e) {
+      throw new RuntimeException(e);
+    } catch (FormatterException e) {
       throw new RuntimeException(e);
     }
   }
