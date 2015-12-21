18b30f9a9ea76798ff1c0b4f776f2597a5883b10
Chris Purcell <Chris.Purcell.39@gmail.com>
2015-12-21 12:10:01 +0000

    Clean up PreconditionsExcerpts after review

--- src/main/java/org/inferred/freebuilder/processor/util/PreconditionExcerpts.java
+++ src/main/java/org/inferred/freebuilder/processor/util/PreconditionExcerpts.java
@@ -1,7 +1,5 @@
 package org.inferred.freebuilder.processor.util;
 
-import static org.inferred.freebuilder.processor.util.PreconditionExcerpts.StateCondition.IS;
-
 import com.google.common.base.Preconditions;
 import com.google.common.escape.Escaper;
 import com.google.common.escape.Escapers;
@@ -14,16 +12,32 @@ public class PreconditionExcerpts {
   private static final Escaper JAVA_STRING_ESCAPER =
       Escapers.builder().addEscape('"', "\"").build();
 
+  /**
+   * Whether to check that a condition {@code IS} or {@code IS_NOT} true.
+   *
+   * <p>Lets us generate code without double negatives (i.e. '!!')
+   */
   public static enum StateCondition {
-    IS, IS_NOT
+    IS(""), IS_NOT("!");
+
+    private final String trueIfConditionHolds;
+
+    private StateCondition(String trueIfConditionHolds) {
+      this.trueIfConditionHolds = trueIfConditionHolds;
+    }
   }
 
   /**
    * Returns an excerpt of the preamble required to emulate an inline call to Guava's
    * {@link Preconditions#checkNotNull(Object)} method.
    *
-   * <p>If you use this, you <b>must</b> also use {@link #checkNotNullInline} to perform the check
-   * inline when possible.
+   * <p>If you use this, you <b>must</b> also use {@link #checkNotNullInline} to allow the check to
+   * be performed inline when possible:
+   *
+   * <pre>code.add(checkNotNullPreamble("value"))
+   *    .addLine("this.property = %s;", checkNotNullInline("value"));</pre>
+   *
+   * @param reference an excerpt containing the reference to pass to the checkNotNull method
    */
   public static Excerpt checkNotNullPreamble(final Object reference) {
     return new Excerpt() {
@@ -36,10 +50,13 @@ public class PreconditionExcerpts {
 
   /**
    * Returns an excerpt equivalent to an inline call to Guava's
-   * {@link Preconditions#checkNotNull(Object)}:
+   * {@link Preconditions#checkNotNull(Object)}.
    *
-   * <p>If you use this, you <b>must</b> also use {@link #checkNotNullPreamble} to perform the
-   * out-of-line check if necessary.
+   * <p>If you use this, you <b>must</b> also use {@link #checkNotNullPreamble} to allow the
+   * check to be performed out-of-line if necessary:
+   *
+   * <pre>code.add(checkNotNullPreamble("value"))
+   *    .addLine("this.property = %s;", checkNotNullInline("value"));</pre>
    *
    * @param reference an excerpt containing the reference to pass to the checkNotNull method
    */
@@ -53,8 +70,11 @@ public class PreconditionExcerpts {
   }
 
   /**
-   * Returns an excerpt equivalent to a call to Guava's
-   * {@link Preconditions#checkNotNull(Object)}:
+   * Returns an excerpt equivalent to Guava's {@link Preconditions#checkNotNull(Object)}:
+   *
+   * <pre>code.add(checkNotNull("key"))
+   *    .add(checkNotNull("value"))
+   *    .addLine("map.put(key, value);");</pre>
    *
    * @param reference an excerpt containing the reference to pass to the checkNotNull method
    */
@@ -71,6 +91,8 @@ public class PreconditionExcerpts {
    * Returns an excerpt equivalent to Guava's
    * {@link Preconditions#checkArgument(boolean, String, Object...)}.
    *
+   * <pre>code.add(checkArgument(IS, "age >= 0", "age must be non-negative (got %s)", "age"));</pre>
+   *
    * @param isOrIsNot whether to negate {@code condition} or not
    * @param condition an excerpt containing the expression to pass to the checkArgument method
    * @param message the error message template to pass to the checkArgument method
@@ -84,13 +106,10 @@ public class PreconditionExcerpts {
     return new Excerpt() {
       @Override
       public void addTo(SourceBuilder code) {
-        boolean addNewlines = excerptIsLong(code, condition);
-        code.add("%s.checkArgument(%s%s%s,%s\"%s\"",
+        code.add("%s.checkArgument(%s%s, \"%s\"",
             Preconditions.class,
-            addNewlines ? "\n" : "",
-            isOrIsNot == IS ? "" : "!",
+            isOrIsNot.trueIfConditionHolds,
             condition,
-            addNewlines ? "\n" : " ",
             JAVA_STRING_ESCAPER.escape(message));
         for (Object arg : args) {
           code.add(", %s", arg);
@@ -103,6 +122,9 @@ public class PreconditionExcerpts {
   /**
    * Returns an excerpt equivalent to Guava's
    * {@link Preconditions#checkState(boolean, String, Object...)}.
+   *
+   * <pre>code.add(checkState(IS, "start < end",
+   *        "start must be before end (got %s and %s)", "start", "end"));</pre>
    *
    * @param isOrIsNot whether to negate {@code condition} or not
    * @param condition an excerpt containing the expression to pass to the checkState method
@@ -117,13 +139,10 @@ public class PreconditionExcerpts {
     return new Excerpt() {
       @Override
       public void addTo(SourceBuilder code) {
-        boolean addNewlines = excerptIsLong(code, condition);
-        code.add("%s.checkState(%s%s%s,%s\"%s\"",
+        code.add("%s.checkState(%s%s, \"%s\"",
             Preconditions.class,
-            addNewlines ? "\n" : "",
-            isOrIsNot == IS ? "" : "!",
+            isOrIsNot.trueIfConditionHolds,
             condition,
-            addNewlines ? "\n" : " ",
             JAVA_STRING_ESCAPER.escape(message));
         for (Object arg : args) {
           code.add(", %s", arg);
@@ -131,15 +150,6 @@ public class PreconditionExcerpts {
         code.add(");\n");
       }
     };
-  }
-
-  private static boolean excerptIsLong(SourceBuilder code, final Object excerpt) {
-    String excerptAsString = SourceStringBuilder
-        .simple(code.getSourceLevel())
-        .add("%s", excerpt)
-        .toString();
-    boolean addNewlines = (excerptAsString.length() > 50);
-    return addNewlines;
   }
 
   private PreconditionExcerpts() {}
