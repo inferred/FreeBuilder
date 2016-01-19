17ab19dac31c52350b02e63f1d4cda9cee1f24f7
Chris Purcell <Chris.Purcell.39@gmail.com>
2016-01-19 23:53:29 +0000

    Skip certain Jackson-annotated properties
    Getters annotated with @JsonAnyGetter, @JsonUnwrapped or @JsonValue should definitely not have @JsonProperty annotations added to the Builder.
    
    Annotating a Map property with @JsonAnyGetter would ideally result in a @JsonAnySetter annotation on the Builder (issue #104), but skipping the @JsonProperty annotations is critical.

--- src/main/java/org/inferred/freebuilder/processor/JacksonSupport.java
+++ src/main/java/org/inferred/freebuilder/processor/JacksonSupport.java
@@ -3,11 +3,14 @@ package org.inferred.freebuilder.processor;
 import static org.inferred.freebuilder.processor.util.ModelUtils.findAnnotationMirror;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableSet;
 
 import org.inferred.freebuilder.processor.Metadata.Property;
 import org.inferred.freebuilder.processor.util.Excerpt;
 import org.inferred.freebuilder.processor.util.QualifiedName;
 import org.inferred.freebuilder.processor.util.SourceBuilder;
+
+import java.util.Set;
 
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.ExecutableElement;
@@ -19,6 +22,11 @@ class JacksonSupport {
       "com.fasterxml.jackson.databind.annotation.JsonDeserialize";
   private static final QualifiedName JSON_PROPERTY =
       QualifiedName.of("com.fasterxml.jackson.annotation", "JsonProperty");
+  /** Annotations which disable automatic generation of JsonProperty annotations. */
+  private static final Set<QualifiedName> DISABLE_PROPERTY_ANNOTATIONS = ImmutableSet.of(
+      QualifiedName.of("com.fasterxml.jackson.annotation", "JsonAnyGetter"),
+      QualifiedName.of("com.fasterxml.jackson.annotation", "JsonUnwrapped"),
+      QualifiedName.of("com.fasterxml.jackson.annotation", "JsonValue"));
 
   public static Optional<JacksonSupport> create(TypeElement userValueType) {
     if (findAnnotationMirror(userValueType, JSON_DESERIALIZE).isPresent()) {
@@ -34,9 +42,21 @@ class JacksonSupport {
     Optional<AnnotationMirror> annotation = findAnnotationMirror(getterMethod, JSON_PROPERTY);
     if (annotation.isPresent()) {
       resultBuilder.addAccessorAnnotations(new AnnotationExcerpt(annotation.get()));
-    } else {
+    } else if (generateDefaultAnnotations(getterMethod)) {
       resultBuilder.addAccessorAnnotations(new JsonPropertyExcerpt(resultBuilder.getName()));
     }
+  }
+
+  private static boolean generateDefaultAnnotations(ExecutableElement getterMethod) {
+    for (AnnotationMirror annotationMirror : getterMethod.getAnnotationMirrors()) {
+      TypeElement annotationTypeElement =
+          (TypeElement) (annotationMirror.getAnnotationType().asElement());
+      QualifiedName annotationType = QualifiedName.of(annotationTypeElement);
+      if (DISABLE_PROPERTY_ANNOTATIONS.contains(annotationType)) {
+        return false;
+      }
+    }
+    return true;
   }
 
   private static class AnnotationExcerpt implements Excerpt {
