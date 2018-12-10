c0bc6852a5e993b78f72086e38a3c8fca8632910
Alice Purcell <Alice.Purcell.39@gmail.com>
2018-12-10 13:17:00 +0000

    Remove unnecessary references to JAVA_8 constant

--- src/test/java/org/inferred/freebuilder/processor/NullableMapperMethodTest.java
+++ src/test/java/org/inferred/freebuilder/processor/NullableMapperMethodTest.java
@@ -48,7 +48,7 @@ public class NullableMapperMethodTest {
   @Parameters(name = "{0}, {1}")
   public static Iterable<Object[]> parameters() {
     List<NamingConvention> conventions = Arrays.asList(NamingConvention.values());
-    List<FeatureSet> features = FeatureSets.WITH_LAMBDAS;
+    List<FeatureSet> features = FeatureSets.ALL;
     return () -> Lists
         .cartesianProduct(conventions, features)
         .stream()
