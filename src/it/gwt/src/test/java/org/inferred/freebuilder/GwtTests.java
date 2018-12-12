65ab050ab563658963fa07caca98e2b1635d7dbf
Alice Purcell <Alice.Purcell.39@gmail.com>
2018-12-12 07:44:54 +0000

    Fix GWT tests to work with Java 8

---
+++ src/it/gwt/src/test/java/org/inferred/freebuilder/GwtTests.java
@@ -0,0 +1,18 @@
+package org.inferred.freebuilder;
+
+import com.google.gwt.junit.tools.GWTTestSuite;
+
+import org.inferred.freebuilder.client.rpc.MapTest;
+import org.inferred.freebuilder.client.rpc.NestedListTest;
+import org.inferred.freebuilder.client.rpc.OptionalTest;
+import org.inferred.freebuilder.client.rpc.StringListTest;
+
+public class GwtTests extends GWTTestSuite {
+
+  public GwtTests() {
+    addTest(new MapTest());
+    addTest(new NestedListTest());
+    addTest(new OptionalTest());
+    addTest(new StringListTest());
+  }
+}
