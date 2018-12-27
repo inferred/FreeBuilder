0f4852725d373ce97d512c8f9b0d43e6821fcf01
Alice Purcell <Alice.Purcell.39@gmail.com>
2018-12-27 11:48:29 +0000

    No need to check for UnsupportedClassVersionError any more

--- src/main/java/org/inferred/freebuilder/processor/util/CompilationUnitBuilder.java
+++ src/main/java/org/inferred/freebuilder/processor/util/CompilationUnitBuilder.java
@@ -20,6 +20,7 @@ import static com.google.common.collect.Iterables.getLast;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.googlejavaformat.java.Formatter;
+import com.google.googlejavaformat.java.FormatterException;
 
 import org.inferred.freebuilder.processor.util.Scope.FileScope;
 import org.inferred.freebuilder.processor.util.feature.FeatureSet;
@@ -122,10 +123,7 @@ public class CompilationUnitBuilder
   public static String formatSource(String source) {
     try {
       return new Formatter().formatSource(source);
-    } catch (UnsupportedClassVersionError e) {
-      // Formatter requires Java 7+; do no formatting in Java 6.
-      return source;
-    } catch (Exception e) {
+    } catch (FormatterException | RuntimeException e) {
       StringBuilder message = new StringBuilder()
           .append("Formatter failed:\n")
           .append(e.getMessage())
