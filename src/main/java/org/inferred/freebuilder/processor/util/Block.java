31a58a2aea9e9a8514c9a33b51a2799b5c4d381a
Alice Purcell <Alice.Purcell.39@gmail.com>
2019-01-17 14:26:56 +0000

    Delete unused Block.declare method

--- src/main/java/org/inferred/freebuilder/processor/util/Block.java
+++ src/main/java/org/inferred/freebuilder/processor/util/Block.java
@@ -1,15 +1,10 @@
 package org.inferred.freebuilder.processor.util;
 
-import static com.google.common.base.Preconditions.checkState;
-import static com.google.common.collect.Maps.newLinkedHashMap;
-
 import org.inferred.freebuilder.processor.util.feature.Feature;
 import org.inferred.freebuilder.processor.util.feature.FeatureType;
 
-import java.util.Map;
-
 /**
- * A Block contains a preamble of lazily-added declarations followed by a body.
+ * A Block encapsulates a method scope, including any parameters it declares.
  */
 public class Block extends Excerpt implements SourceBuilder {
 
@@ -21,63 +16,10 @@ public class Block extends Excerpt implements SourceBuilder {
     return new Block(parent, methodScope);
   }
 
-  private final Map<String, String> variableNames = newLinkedHashMap();
-  private final Map<String, Excerpt> declarations = newLinkedHashMap();
-  private final SourceStringBuilder declarationsBlock;
   private final SourceStringBuilder body;
 
   private Block(SourceBuilder parent, Scope newScope) {
-    declarationsBlock = parent.subScope(newScope);
     body = parent.subScope(newScope);
-  }
-
-  /**
-   * Declare a variable, preferably named {@code preferredName} but renamed if necessary to avoid
-   * collisions, in this block's preamble, returning an Excerpt to use it. Duplicate declarations
-   * will be elided.
-   *
-   * @throws IllegalStateException if {@code preferredName} has already been added to this block
-   *     with a different declaration
-   */
-  public Excerpt declare(Excerpt typeAndPreamble, String preferredName, Excerpt value) {
-    String name;
-    if (variableNames.containsKey(preferredName)) {
-      name = variableNames.get(preferredName);
-      Excerpt declaration = Excerpts.add("%s %s = %s;%n", typeAndPreamble, name, value);
-      Excerpt existingDeclaration = declarations.get(name);
-      checkState(declaration.equals(existingDeclaration),
-          "Incompatible declaration for '%s': %s vs %s",
-          name,
-          declaration,
-          existingDeclaration);
-    } else {
-      name = pickName(preferredName);
-      variableNames.put(preferredName, name);
-      body.scope().add(new VariableName(name));
-      Excerpt declaration = Excerpts.add("%s %s = %s;%n", typeAndPreamble, name, value);
-      declarations.put(name, declaration);
-      declarationsBlock.add(declaration);
-    }
-    return Excerpts.add("%s", name);
-  }
-
-  private String pickName(String preferredName) {
-    if (!nameCollides(preferredName)) {
-      return preferredName;
-    }
-    if (!nameCollides("_" + preferredName)) {
-      return "_" + preferredName;
-    }
-    int suffix = 2;
-    while (nameCollides("_" + preferredName + suffix)) {
-      suffix++;
-    }
-    return "_" + preferredName + suffix;
-  }
-
-  private boolean nameCollides(String name) {
-    return body.scope().contains(new VariableName(name))
-        || body.scope().contains(new FieldAccess(name));
   }
 
   @Override
@@ -120,12 +62,11 @@ public class Block extends Excerpt implements SourceBuilder {
 
   @Override
   public void addTo(SourceBuilder source) {
-    source.add("%s%s", declarationsBlock, body);
+    source.add("%s", body);
   }
 
   @Override
   protected void addFields(FieldReceiver fields) {
-    fields.add("declarations", declarations);
     fields.add("body", body.toString());
   }
 }
